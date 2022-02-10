db50eb10d90f9131f19958fdbcdeb8c8fd846ee4
xieqingfeng <btkinghome@gmail.com>
2022-02-10 15:22:03 +0800

    取消 启动连接mysql

--- main.go
+++ main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-
-	"github.com/EDDYCJY/go-gin-example/models"
 	"github.com/EDDYCJY/go-gin-example/pkg/gredis"
 	"github.com/EDDYCJY/go-gin-example/pkg/logging"
 	"github.com/EDDYCJY/go-gin-example/pkg/setting"
-	"github.com/EDDYCJY/go-gin-example/routers"
 	"github.com/EDDYCJY/go-gin-example/pkg/util"
+	"github.com/EDDYCJY/go-gin-example/routers"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func init() {
+	//需要修改，放到启动之后。
 	setting.Setup()
-	models.Setup()
+	//models.Setup()
 	logging.Setup()
 	gredis.Setup()
 	util.Setup()
